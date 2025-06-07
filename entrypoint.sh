e4f95528de88dd6f6fd9ed2a3fddb287ed964d12
Darkluke1111 <lukas.gromer@freenet.de>
2025-06-07 15:59:38 +0200

    fixed vis version evn var

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 cd ${VINTAGE_STORY}
 
-wget -q "${STABLE_URL}${VS_VERSION}.tar.gz"
-tar xzf vs_server_linux-x64_${VS_VERSION}.tar.gz
-rm vs_server_linux-x64_${VS_VERSION}.tar.gz
+wget -q "${STABLE_URL}${INPUT_VS_VERSION}.tar.gz"
+tar xzf vs_server_linux-x64_${INPUT_VS_VERSION}.tar.gz
+rm vs_server_linux-x64_${INPUT_VS_VERSION}.tar.gz
 
 cd /github/workspace
 for filename in *; do
