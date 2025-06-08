414302e9bd261cbd4666e5d58988398a057d1da6
Darkluke1111 <lukas.gromer@freenet.de>
2025-06-08 10:19:20 +0200

    exporting path to result in env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,9 @@ rm vs_server_linux-x64_${INPUT_VS_VERSION}.tar.gz
 cd /github/workspace
 
 dotnet run --project ./${filename}/ZZCakeBuild/CakeBuild.csproj -- "$@"
+
+for file in Releases/*.zip; do
+    echo "RESULT=$file" >> ${GITHUB_ENV} 
+done
+
+
