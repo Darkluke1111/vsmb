72745289ae68f693a3c04e25c3200a2c0f8ac5ef
Darkluke1111 <lukas.gromer@gmx.de>
2026-05-04 15:11:59 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,14 +8,17 @@ rm vs_server_linux-x64_${INPUT_VS_VERSION}.tar.gz
 
 cd /github/workspace
 
+modVersion= find . -name modinfo.json | xargs cat | jq -r '.version'
+echo "MOD_VERSION=$modVersion"
+echo "MOD_VERSION=$modVersion" >> ${GITHUB_ENV}
+
 dotnet run --project ./ZZCakeBuild/CakeBuild.csproj -- "$@"
 
 for file in Releases/*.zip; do
     echo "RESULT=$file" >> ${GITHUB_ENV} 
 done
 
-MOD_VERSION= find . -name modinfo.json | xargs cat | jq -r '.version'
-echo "MOD_VERSION=$MOD_VERSION" >> ${GITHUB_ENV}
+
 
 
 
